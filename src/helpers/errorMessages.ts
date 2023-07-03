b350a73ecd8305b3d4b35d9ddcf9ebf7a360ea98
mikalai <mikalai.hurynovich@itechart-group.com>
2023-07-03 11:21:54 +0300

    feat: added helpers

---
+++ src/helpers/errorMessages.ts
@@ -0,0 +1,9 @@
+export const MessageErr = {
+  isNotValidID: (id: string) => ({ error: `this Id: ${id} is not valid` }),
+  isNotUserID: (id: string) => ({ error: `there is no user with this id: ${id}` }),
+  failed: (method: 'create' | 'update') => ({ error: `failed to ${method} user` }),
+  notFound: { error: 'Not Found' },
+  notID: { message: `id not specified` },
+  urlExists: { message: 'this url does not exist' },
+  balancer: { message: 'the load balancer accepts requests on port 4000' }
+}
